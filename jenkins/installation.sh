a8cd22774cc7e4b3c0f36c300e080e3d5eb09a38
jaimesalas <jaime.salas@lemoncode.net>
2023-03-08 18:18:55 +0100

    added new workflow

--- jenkins/installation.sh
+++ jenkins/installation.sh
@@ -27,3 +27,17 @@ docker run \
     --volume jenkins-data:/var/jenkins_home \
     --volume jenkins-docker-certs:/certs/client:ro \
     jenkins-blueocean
+
+docker run \
+    --name jenkins-blueocean \
+    --restart=on-failure \
+    --detach \
+    --network jenkins \
+    --env DOCKER_HOST=tcp://docker:2376 \
+    --env DOCKER_CERT_PATH=/certs/client \
+    --env DOCKER_TLS_VERIFY=1 \
+    --publish 8080:8080 \
+    --publish 5000:5000 \
+    --volume jenkins-data:/var/jenkins_home \
+    --volume jenkins-docker-certs:/certs/client:ro \
+    jenkins-blueocean-plain
