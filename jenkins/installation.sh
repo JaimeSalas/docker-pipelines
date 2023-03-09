997fc69a1a1080d5585cde6287e32fd734905ccb
jaimesalas <jaime.salas@lemoncode.net>
2023-03-09 10:09:43 +0100

    added foo

--- jenkins/installation.sh
+++ jenkins/installation.sh
@@ -1,4 +1,4 @@
-docker network create jenkins 
+docker network create jenkins
 
 docker run \
     --name jenkins-docker \
@@ -23,7 +23,7 @@ docker run \
     --env DOCKER_CERT_PATH=/certs/client \
     --env DOCKER_TLS_VERIFY=1 \
     --publish 8080:8080 \
-    --publish 5000:5000 \
+    --publish 50000:50000 \
     --volume jenkins-data:/var/jenkins_home \
     --volume jenkins-docker-certs:/certs/client:ro \
     jenkins-blueocean
